0cb1730d0cc0efd18d7c9b3a150c7b6712f2963c
zhangjiabao <greedisgood19881215@126.com>
2018-11-07 22:06:36 +0800

    更多页面：定义通用cell组件，完成页面布局

---
+++ components/More/MTCommonCell.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Image, TouchableOpacity, Alert, Switch} from 'react-native';
+
+
+export default class CommonCell extends Component {
+
+  static defaultProps = {
+    title: '',        //标题
+    isSwitch: false,  //是否需要显示开关
+    subtitle: ''      //子标题
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: false,  //开关状态
+    }
+    this.renderRightView = this.renderRightView.bind(this);
+  }
+
+  render() {
+    return (
+      <TouchableOpacity onPress={()=>{Alert.alert(this.props.title)}} activeOpacity={0.8}>
+        <View style={styles.container}>
+          <Text style={{fontSize:16, marginLeft:10}}>{this.props.title}</Text>
+          {this.renderRightView()}
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  renderRightView() {
+    if (this.props.isSwitch) {
+      return (
+        <Switch
+          style={{marginRight: 10}}
+          value={this.state.isOn == true}
+          onValueChange={()=>{
+            this.setState({
+              isOn: !this.state.isOn
+            });
+          }}
+        />
+      );
+    } else {
+      return (
+        <View style={{flexDirection:'row',justifyContent:'flex-end', alignItems:'center'}}>
+          <Text style={{fontSize:12, marginRight:5}}>{this.props.subtitle}</Text>
+          <Image
+            style={{width:15, height:22, marginRight:10}}
+            source={require('../../images/home_arrow.png')}
+          />
+        </View>
+      );
+    }
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 44,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomColor:'#e8e8e8',
+    borderBottomWidth: 0.5,
+  }
+});
--- components/More/MTMore.js
+++ components/More/MTMore.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, TouchableOpacity, Dimensions, Alert} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, TouchableOpacity, Dimensions, Alert, ScrollView} from 'react-native';
+import CommonCell from './MTCommonCell';
 
 const {width, height} = Dimensions.get('window');
 const IsIphoneX = (Platform.OS == 'ios' && width == 375 && height == 812);
@@ -8,17 +9,44 @@ export default class More extends Component {
   render() {
     return (
       <View style={styles.container}>
+
+        {/*navBar*/}
         <View style={styles.navBarStyle}>
           <View style={styles.navImgWrapper}>
             <TouchableOpacity
               onPress={()=>{Alert.alert('touch more setting'); }}
               activeOpacity = {0.5}
             >
-              <Image style={styles.navImgStyle} source={require('../../images/icon_mine_setting.png')} />
+              <Image style={styles.navImgStyle} source={require('../../images/icon_more_setting.png')} />
             </TouchableOpacity>
           </View>
           <Text style={styles.navTitleStyle}>更多</Text>
         </View>
+
+        {/*ScrollView*/}
+        <ScrollView>
+          <View style={{marginTop:10}}>
+            <CommonCell title="扫一扫" />
+          </View>
+          <View style={{marginTop:10}}>
+            <CommonCell title="省流量模式" isSwitch={true} />
+            <CommonCell title="消息提醒" />
+            <CommonCell title="邀请好友" />
+            <CommonCell title="清空缓存" subtitle="2.98MB" />
+          </View>
+          <View style={{marginTop:10}}>
+            <CommonCell title="意见反馈" />
+            <CommonCell title="问卷调查" />
+            <CommonCell title="支付帮助" />
+            <CommonCell title="网络诊断" />
+            <CommonCell title="关于码团" />
+            <CommonCell title="我要应聘" />
+          </View>
+          <View style={{marginTop:10}}>
+            <CommonCell title="精品应用" />
+          </View>
+        </ScrollView>
+
       </View>
     );
   }
@@ -27,6 +55,7 @@ export default class More extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+    backgroundColor:'#e8e8e8'
 	},
   navBarStyle: {
     backgroundColor: 'rgb(255, 96, 0)',
